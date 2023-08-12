5bee39f9b1b203300736ff06ae039dab8142237e
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2023-08-12 10:49:03 +0200

    Update user - added properties.

--- user.h
+++ user.h
@@ -8,12 +8,21 @@
 class User : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString username READ getUsername WRITE setUsername NOTIFY created FINAL)
     Q_PROPERTY(QString password READ getPassword WRITE setPassword NOTIFY created FINAL)
+    Q_PROPERTY(QString id READ getId WRITE setId NOTIFY created FINAL)
+
+
 public:
     explicit User(QObject *parent = nullptr);
 
     QString getPassword();
+    QString getUsername();
+    QString getId();
+
     void setPassword(QString password);
+    void setUsername(QString username);
+    void setId(QString id);
 
 signals:
     void created();
@@ -22,6 +31,8 @@ public slots:
 
 private:
     QString _password;
+    QString _username;
+    QString _id;
 
 };
 
