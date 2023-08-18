4c54e3259b302de59a3c5468a4469b914dba278d
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2023-08-18 18:06:27 +0200

    Update contact model - code review.

--- contact.cpp
+++ contact.cpp
@@ -1,63 +1,24 @@
 #include "contact.h"
 
 Contact::Contact(QObject *parent) : QObject{parent} {
-  this->_owner_id = {};
-  this->_user_id = {};
-  this->_username = {};
   this->_created = QDateTime();
 }
 
-Contact::Contact(QString owner_id, QString user_id, QString username,
-                 QDateTime created, QObject *parent)
+Contact::Contact(int ownerId, User *contact, QDateTime created, QObject *parent)
     : Contact{parent} {
-  this->_user_id = user_id;
-  this->_owner_id = owner_id;
-  this->_username = username;
+  this->_owner_id = ownerId;
+  this->_contact = contact;
   this->_created = created;
 }
 
-QString Contact::getUsername() { return this->_username; }
-void Contact::setUsername(QString value) {
-  this->_username = value;
-  emit this->usernameChanged();
-}
+int Contact::getOnwerId() { return this->_owner_id; }
 
-QString Contact::getOwnerId() { return this->_owner_id; }
-void Contact::setOwnerId(QString value) {
-  this->_owner_id = value;
-  emit this->ownerIdChanged();
-}
+void Contact::setOnwerId(int value) { this->_owner_id = value; }
 
-QString Contact::getUserId() { return this->_user_id; }
-void Contact::setUserId(QString value) {
-  this->_user_id = value;
-  emit this->userIdChanged();
-}
+User *Contact::getContact() { return this->_contact; }
+
+void Contact::setContact(User *value) { this->_user = value; }
 
 QDateTime Contact::getCreated() { return this->_created; }
-void Contact::setCreated(QDateTime value) {
-  this->_created = value;
-  emit this->created();
-}
 
-void Contact::remove() {
-  // TODO: make remove from db;
-  emit this->removed();
-}
-
-bool Contact::create() {
-  // TODO: this is going to server;
-  SqlContact *contact = new SqlContact(this);
-  contact->setUserId(this->_user_id.toInt());
-  contact->setOwnerId(this->_owner_id.toInt());
-  contact->setUsername(this->_username);
-  contact->setCreated(QDateTime::currentDateTime());
-
-  if (!contact->createContact()) {
-    emit this->creatingFail();
-    return false;
-  }
-
-  emit this->successfullyCreated();
-  return true;
-};
+void Contact::setCreated(QDateTime value) { this->_created = value; }
--- contact.h
+++ contact.h
@@ -1,57 +1,37 @@
 #ifndef CONTACT_H
 #define CONTACT_H
 
-#include <QObject>
 #include <QDateTime>
+#include <QObject>
 
-#include "sqlcontact.h"
-#include "sqluser.h"
+#include "user.h"
 
-class Contact : public QObject
-{
+class User;
+class Contact : public QObject {
   Q_OBJECT
-public:
-  explicit Contact(QObject *parent = nullptr);
-  Contact(QString owner_id, QString user_id, QString username, QDateTime created, QObject *parent = nullptr);
+      public:
+               explicit Contact(QObject *parent = nullptr);
+  Contact(int owner_id, User *contact, QDateTime created,
+          QObject *parent = nullptr);
 
-  Q_PROPERTY(QString userId READ getUserId WRITE setUserId NOTIFY userIdChanged FINAL)
-  Q_PROPERTY(QString ownerId READ getOwnerId WRITE setOwnerId NOTIFY ownerIdChanged FINAL)
-  Q_PROPERTY(QString username READ getUsername WRITE setUsername NOTIFY
-                 usernameChanged FINAL);
-  Q_PROPERTY(QDateTime created READ getCreated WRITE setCreated NOTIFY
-                 createdChanged FINAL)
+  Q_PROPERTY(int ownerId READ getOwnerId WRITE setOwnerId)
+  Q_PROPERTY(User *contact READ getContact WRITE setContact)
+  Q_PROPERTY(QDateTime created READ getCreated WRITE setCreated)
 
-  // TODO: avatar property
-  void setUsername(QString value);
-  QString getUsername();
+  int getOnwerId();
+  void setOnwerId(int value);
 
-  void setOwnerId(QString value);
-  QString getOwnerId();
-
-  void setUserId(QString value);
-  QString getUserId();
+  User *getContact();
+  void setContact(User *value);
 
   void setCreated(QDateTime value);
   QDateTime getCreated();
 
 signals:
-  void userUpdated();
-  void created();
-  void removed();
-  void usernameChanged();
-  void createdChanged();
-  void userIdChanged();
-  void ownerIdChanged();
-  void idChanged();
-  void successfullyCreated();
-  void creatingFail();
-
-public slots:
-  void remove();
-  bool create();
 
 private:
-  QString _owner_id, _user_id, _username;
+  int _owner_id;
+  User *_contact;
   QDateTime _created;
 };
 
