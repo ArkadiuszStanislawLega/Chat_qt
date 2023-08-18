73cc8da08f5a25ebe52978653b92da90b0a1cb3e
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2023-08-18 15:18:20 +0200

    Update sqlcontact - adding contact and read contact.

--- sqlcontact.cpp
+++ sqlcontact.cpp
@@ -8,9 +8,35 @@ SqlContact::SqlContact(QObject *parent) : QObject{parent} {
   QDateTime _created = QDateTime();
 }
 
-bool SqlContact::createContact() { return false; }
+bool SqlContact::createContact() {
+  QVector<QPair<QString, QString>> args = {
+      {*ID_COLUMN_NAME, QString::number(this->_contact_id)},
+      {*ID_OWNER_ID_COLUMN_NAME, QString::number(this->_owner_id)},
+      {*ID_USER_ID_COLUMN_NAME, QString::number(this->_user_id)},
+      {*USERNAME_COLUMN_NAME, this->_username},
+      {*CREATED_DATE_COLUMN_NAME, this->_created.toString()},
+  };
+  return DbManager::create(*CONTACTS_TABLE_NAME, args);
+}
 
-bool SqlContact::readContact() { return false; }
+bool SqlContact::readContact() {
+  QString username, id_contact, id_owner, id_user, created;
+  QVector<QPair<QString, QString *>> args = {
+      {*ID_COLUMN_NAME, &id_contact},
+      {*ID_OWNER_ID_COLUMN_NAME, &id_owner},
+      {*ID_USER_ID_COLUMN_NAME, &id_user},
+      {*USERNAME_COLUMN_NAME, &username},
+      {*CREATED_DATE_COLUMN_NAME, &created}};
+  if (DbManager::read(*CONTACTS_TABLE_NAME, args)) {
+    this->setContactId(id_contact.toInt());
+    this->setOwnerId(id_owner.toInt());
+    this->setUserId(id_user.toInt());
+    this->setUsername(username);
+    this->setCreated(QDateTime::fromString(created));
+    return true;
+  }
+  return false;
+}
 
 bool SqlContact::updateContact() { return false; }
 
