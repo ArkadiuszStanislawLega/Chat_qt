80b2c5fb43130839af73b15ee04ac3badc0f6e9f
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2023-08-27 13:37:34 +0200

    Update sql message - added properties.

--- sqlmessage.cpp
+++ sqlmessage.cpp
@@ -2,7 +2,8 @@
 
 /* TODO: Implement this.
  * Insert message query.
- * insert into Messages (contact_id, author_id, message, sended_time) VALUES(1,1,"druga wiadomosc",  "2023-10-12T12:10:00");
+ * insert into Messages (contact_id, author_id, message, sended_time)
+VALUES(1,1,"druga wiadomosc",  "2023-10-12T12:10:00");
  * /
 
  /* TODO: Implement this.
@@ -23,8 +24,28 @@ where Messages.author_id = 1
 ORDER BY Messages.sended_time DESC	;
  */
 
-SqlMessage::SqlMessage(QObject *parent)
-    : QObject{parent}
-{
+SqlMessage::SqlMessage(QObject *parent) : QObject{parent} {}
 
+int SqlMessage::getAuthorId() { return this->_author_id; }
+void SqlMessage::setAuthorId(int value) { this->_author_id = value; }
+
+int SqlMessage::getReceiverId() { return this->_receiver_id; }
+void SqlMessage::setReceiverId(int value) { this->_receiver_id = value; }
+
+QString SqlMessage::getText() { return this->_text; }
+void SqlMessage::setText(QString value) { this->_text = value; }
+
+QString SqlMessage::getAuthorUsername() { return this->_author_username; }
+void SqlMessage::setAuthorUsername(QString value) {
+  this->_author_username = value;
+}
+
+QString SqlMessage::getReceiverUsername() { return this->_receiver_username; }
+void SqlMessage::setReceiverUsername(QString value) {
+  this->_receiver_username = value;
+}
+
+QDateTime SqlMessage::getSentTimestamp() { return this->_sentTimestamp; }
+void SqlMessage::setSentTimestamp(QDateTime value) {
+  this->_sentTimestamp = value;
 }
--- sqlmessage.h
+++ sqlmessage.h
@@ -2,15 +2,40 @@
 #define SQLMESSAGE_H
 
 #include <QObject>
+#include <QDateTime>
 
-class SqlMessage : public QObject
-{
+class SqlMessage : public QObject {
+
+
   Q_OBJECT
-      public:
-               explicit SqlMessage(QObject *parent = nullptr);
+private:
+  int _author_id, _receiver_id;
+  QString _text, _author_username, _receiver_username;
+  QDateTime _sentTimestamp;
+
+public:
+  explicit SqlMessage(QObject *parent = nullptr);
+
+  int getAuthorId();
+  void setAuthorId(int);
+
+  int getReceiverId();
+  void setReceiverId(int);
+
+  QString getText();
+  void setText(QString);
+
+  QString getAuthorUsername();
+  void setAuthorUsername(QString);
+
+  QString getReceiverUsername();
+  void setReceiverUsername(QString);
+
+  QDateTime getSentTimestamp();
+  void setSentTimestamp(QDateTime);
+
 
 signals:
-
 };
 
 #endif // SQLMESSAGE_H
