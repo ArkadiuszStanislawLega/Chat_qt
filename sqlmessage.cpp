b1d83efc7309cea2dc906ab61eaf43f8b5007f36
Arkadiusz Lega <arkadiusz.stanislaw.lega@gmail.com>
2023-09-08 11:03:40 +0200

    Update sql message = code review, added docs.

--- sqlmessage.cpp
+++ sqlmessage.cpp
@@ -1,14 +1,5 @@
 #include "sqlmessage.h"
 
-bool SqlMessage::executeQuery(QSqlQuery &query) {
-	qDebug() << "Sql Message executing: " << query.lastQuery();
-	if (!query.exec()) {
-		qDebug() << query.lastError() << query.lastQuery();
-		return false;
-	}
-	return true;
-}
-
 SqlMessage::SqlMessage(QObject *parent)
 	: QObject{parent} {
 	this->_author_id = 0;
@@ -16,6 +7,20 @@ SqlMessage::SqlMessage(QObject *parent)
 	this->_author_username = "";
 	this->_sentTimestamp = QDateTime::currentDateTime();
 	this->_text = "";
+}
+
+/*!
+ * \brief SqlMessage::executeQuery Executing query inserted as argument to function. 
+ * \param query Query to execute in database.
+ * \return True if executing successfully complited.
+ */
+bool SqlMessage::executeQuery(QSqlQuery &query) {
+	qDebug() << "Sql Message executing: " << query.lastQuery();
+	if (!query.exec()) {
+		qDebug() << query.lastError() << query.lastQuery();
+		return false;
+	}
+	return true;
 }
 
 int SqlMessage::getContactId() const {
@@ -53,7 +58,14 @@ void SqlMessage::setSentTimestamp(QDateTime value) {
 	this->_sentTimestamp = value;
 }
 
+/*!
+ * \brief SqlMessage::createMessage Adding message to database if all required ids are not 0 and message is not empty.
+ * \return True if message is successfully added to database.
+ */
 bool SqlMessage::createMessage() {
+	if (this->_contact_id <= 0 || this->_author_id <= 0 || this->_text.isEmpty())
+		return false;
+
 	QSqlQuery query;
 	query.prepare("INSERT INTO " + *MESSAGES_TABLE_NAME + "(" + *CONTACT_ID_COLUMN_NAME + ","
 				  + *AUTHOR_ID_COLUMN_NAME + "," + *TEXT_COLUMN_NAME + ","
@@ -67,25 +79,28 @@ bool SqlMessage::createMessage() {
 
 	return this->executeQuery(query);
 }
-/* TODO: Implement this.
- * Selected messages for contact query.
-select
-            sender.id as first_id,
-            sender.username as first_username,
-            reciver.id as second_id,
-            reciver.username as second_username,
-            Messages.author_id,
-            Messages.text,
-            Messages.sent_timestamp
-            from Messages
-INNER JOIN Contacts ON Contacts.id = Messages.contact_id
-INNER JOIN Users as sender ON sender.id = Contacts.first_user_id
-INNER JOIN Users as reciver ON reciver.id = Contacts.second_user_id
-where Contacts.id = 1
-ORDER BY Messages.sent_timestamp DESC;
-*/
 
+/*!
+ * \brief SqlMessage::buildQueryReadMessages building query to select all messages connected with Contact.
+ * \return Query selecting all messages connected with Contact.
+ */
 QString SqlMessage::buildQueryReadMessages() {
+	/*
+    select
+                sender.id as first_id,
+                sender.username as first_username,
+                reciver.id as second_id,
+                reciver.username as second_username,
+                Messages.author_id,
+                Messages.text,
+                Messages.sent_timestamp
+                from Messages
+    INNER JOIN Contacts ON Contacts.id = Messages.contact_id
+    INNER JOIN Users as sender ON sender.id = Contacts.first_user_id
+    INNER JOIN Users as reciver ON reciver.id = Contacts.second_user_id
+    where Contacts.id = 1
+    ORDER BY Messages.sent_timestamp DESC;
+    */
 	return "SELECT " + *MESSAGES_TABLE_NAME + "." + *ID_COLUMN_NAME + ", " + *CONTACT_ID_COLUMN_NAME
 		   + "," + *AUTHOR_ID_COLUMN_NAME + ", " + *USERS_TABLE_NAME + "." + *USERNAME_COLUMN_NAME
 		   + ", " + *TEXT_COLUMN_NAME + ", " + *SENT_TIMESTAMP_COLUMN_NAME + " FROM "
@@ -95,6 +110,11 @@ QString SqlMessage::buildQueryReadMessages() {
 		   + *CONTACT_ID_COLUMN_NAME + " ORDER BY " + *SENT_TIMESTAMP_COLUMN_NAME + " DESC;";
 }
 
+/*!
+ * \brief SqlMessage::getMessageFromQuery Reciving message from query inserted to argument.
+ * \param query Query selecting message.
+ * \return Message from query.
+ */
 Message *SqlMessage::getMessageFromQuery(QSqlQuery &query) {
 	int messages_id_column, contact_id_column, author_username_column, author_id_column,
 		text_column, sent_timestamp_column;
@@ -118,7 +138,14 @@ Message *SqlMessage::getMessageFromQuery(QSqlQuery &query) {
 	return message;
 }
 
+/*!
+ * \brief SqlMessage::readMessages Reading messages from database if contact id is setted.
+ * \return QList of messages connected with Contact id.
+ */
 QList<Message *> SqlMessage::readMessages() {
+	if (this->_contact_id <= 0)
+		return {};
+
 	QSqlQuery query;
 	query.prepare(this->buildQueryReadMessages());
 	query.bindValue(":" + *CONTACT_ID_COLUMN_NAME, this->_contact_id);
@@ -132,6 +159,7 @@ QList<Message *> SqlMessage::readMessages() {
 	}
 	return messages;
 }
+
 bool SqlMessage::updateMessage() {
 	return false;
 }
